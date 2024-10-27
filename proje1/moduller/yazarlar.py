9b36484588eaf206c7d50188a5976cd6522f7670
Buse <busaydogdu77@gmail.com>
2024-10-27 13:39:46 +0300

    Add files via upload

--- proje1/moduller/yazarlar.py
+++ proje1/moduller/yazarlar.py
@@ -1,19 +1,23 @@
-def cizimmenu():
+def yazarmenu():
     print("\033[1;32;40m")
     #print("╔"+"═"*20+"╗")
     print("╔═════════════════════╗")
-    print("║\033[1;31;40m   ÇİZİMLER      \033[1;32;40m  ║")
+    print("║\033[1;31;40m   YAZARLAR    \033[1;32;40m  ║")
     print("║                     ║")
-    print("║  1-Kare             ║")
-    print("║  2-Üçgen            ║")
-    print("║  3-Dikdörtgen       ║")
-    print("║  4-Daire            ║")
-    print("║  5-Yamuk            ║")
-    print("║  6-Beşgen           ║")
-    print("║  7-Altıgen          ║")
+    print("║  1-Ahmet Ümit       ║")
+    print("║  2-Sabahattin Ali   ║")
+    print("║  3-Orhan Pamuk      ║")
+    print("║  4-Yaşar Kemal      ║")
+    print("║  5-Oğuz Atay        ║")
+    print("║  6-Rifat Ilgaz      ║")
+    print("║  7-Zülfü Livaneli   ║")
     print("║  8-                 ║")
     print("║  9-                 ║")
     print("║  10-                ║")
     print("║                     ║")
     print("║    Seçimiz nedir?   ║")
     print("╚═════════════════════╝")
+
+    if secim == "1":
+        print("Ahmet Ümit'i seçtiniz")
+        
