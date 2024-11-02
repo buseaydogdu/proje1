e3c50601ec930e48f6ce418923b3b95c2d84e0a9
Buse <busaydogdu77@gmail.com>
2024-11-02 13:04:04 +0300

    Add files via upload

---
+++ proje1/PROJE1.CONSOLE/moduller/yazarlar.py
@@ -0,0 +1,25 @@
+def yazarmenu():
+    print("\033[1;32;40m")
+    #print("╔"+"═"*20+"╗")
+    print("╔═════════════════════╗")
+    print("║\033[1;31;40m   YAZARLAR    \033[1;32;40m  ║")
+    print("║                     ║")
+    print("║  1-Ahmet Ümit       ║")
+    print("║  2-Sabahattin Ali   ║")
+    print("║  3-Orhan Pamuk      ║")
+    print("║  4-Yaşar Kemal      ║")
+    print("║  5-Oğuz Atay        ║")
+    print("║  6-Rifat Ilgaz      ║")
+    print("║  7-Zülfü Livaneli   ║")
+    print("║  8-                 ║")
+    print("║  9-                 ║")
+    print("║  10-                ║")
+    print("║                     ║")
+    print("║    Seçimiz nedir?   ║")
+    print("╚═════════════════════╝")
+
+    secim = input("Seçiminiz nedir?")
+    
+    if secim == "1":
+        print("Ahmet Ümit'i seçtiniz")
+        
---
+++ proje1/PROJE1.CONSOLE/uygulama.py
@@ -0,0 +1,64 @@
+import moduller.aylar
+import moduller.burclar
+import moduller.cizimler
+import moduller.hesapmak
+import moduller.oyunlar
+import moduller.renkler
+import moduller.yazarlar
+
+def uygulamamenu():
+    print("\033[1;32;40m")
+    #print("╔"+"═"*20+"╗")
+    print("╔═════════════════════╗")
+    print("║\033[1;31;40m   UYGULAMALAR     \033[1;32;40m  ║")
+    print("║                     ║")
+    print("║  1-Hesap makinesi   ║")
+    print("║  2-Çizimler         ║")
+    print("║  3-Aylar            ║")
+    print("║  4-Oyunlar          ║")
+    print("║  5-Burçlar          ║")
+    print("║  6-Renkler          ║")
+    print("║  7-Yazarlar         ║")
+    print("║  8-                 ║")
+    print("║  9-                 ║")
+    print("║  10-                ║")
+    print("║                     ║")
+    print("║    Seçimiz nedir?   ║")
+    print("╚═════════════════════╝")
+
+    secim = input()
+
+    if secim =="1":
+        print("Hesap makinesi uygulamasını seçtiniz")
+        moduller.hesapmak.hesapmenu()
+        uygulamamenu()
+    elif secim =="2":
+        print("Çizimler uygulamasını seçtiniz.")
+        moduller.cizimler.cizimmenu()
+        uygulamamenu()
+    elif secim =="3":
+        print("Aylar uygulmasını seçtiniz.")
+        moduller.aylar.aylarmenu()
+        uygulamamenu()    
+    elif secim =="4":
+        print("Oyunlar uygulmasını seçtiniz.")
+        moduller.oyunlar.oyunlarmenu()
+        uygulamamenu()  
+    elif secim =="5":
+        print("Burçlar uygulmasını seçtiniz.")
+        moduller.burclar.burcmenu()
+        uygulamamenu()    
+    elif secim =="6":
+        print("Renkler uygulmasını seçtiniz.")
+        moduller.renkler.renklermenu()
+        uygulamamenu()    
+    else :
+        print("Yazarlar uygulmasını seçtiniz.")
+        moduller.yazarlar.yazarlarmenu()
+        uygulamamenu() 
+
+uygulamamenu()
+    
+
+    
+        
