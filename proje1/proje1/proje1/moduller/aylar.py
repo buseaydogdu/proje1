0de029415167860f839b22cf5244fc7b50fdb83b
Buse <busaydogdu77@gmail.com>
2024-11-02 12:57:01 +0300

    Add files via upload
    PROJE1.CONSOLE

--- proje1/proje1/proje1/moduller/aylar.py
+++ proje1/proje1/proje1/moduller/aylar.py
@@ -1,25 +1,28 @@
-def aylarmenu():
-    print("\033[1;32;40m")
-            #print("╔"+"═"*20+"╗")
-    print("╔═════════════════════╗")
-    print("║\033[1;31;40m   AYLAR    \033[1;32;40m  ║")
-    print("║                     ║")
-    print("║  1-Ocak             ║")
-    print("║  2-Şubat            ║")
-    print("║  3-Mart             ║")
-    print("║  4-Nisan            ║")
-    print("║  5-Mayıs            ║")
-    print("║  6-Haziran          ║")
-    print("║  7-Temmuz           ║")
-    print("║  8-Ağustos          ║")
-    print("║  9-Eylül            ║")
-    print("║  10-Ekim            ║")
-    print("║                     ║")
-    print("║    Seçimiz nedir?   ║")
-    print("╚═════════════════════╝")
+import moduller.burclar
+def aylarmenu():   
+        print("\033[1;32;40m")
+        #print("╔"+"═"*20+"╗")
+        print("╔═════════════════════╗")
+        print("║\033[1;31;40m   AYLAR           \033[1;32;40m  ║")
+        print("║                     ║") 
+        print("║  1-Ocak             ║")
+        print("║  2-Şubat            ║")
+        print("║  3-Mart             ║")
+        print("║  4-Nisan            ║")
+        print("║  5-Mayis            ║")
+        print("║  6-Haziran          ║")
+        print("║  7-Temmuz           ║")
+        print("║  8-Ağustos          ║")
+        print("║  9-Eylül            ║")
+        print("║  10-Ekim            ║")
+        print("║                     ║")
+        print("║    Seçimiz nedir?   ║")
+        print("╚═════════════════════╝")
 
-    if secim == "1":
-        print("Ocak ayını seçtiniz")
-        uygulamalar.burclarmenu()
-    if secim == "2":
-        print("Şubat ayını seçtiniz")
+        secim=input("Seçiminiz nedir?")
+
+        if secim == "1":
+                print("Ocak ayını seçtiniz")
+                moduller.burclar.burclarmenu()
+        if secim == "2":
+                print("Şubat ayını seçtiniz")
--- proje1/proje1/proje1/uygulama.py
+++ proje1/proje1/proje1/uygulama.py
@@ -1,4 +1,11 @@
-import uygulamalar.uygulamalarmenu
+import moduller.aylar
+import moduller.burclar
+import moduller.cizimler
+import moduller.hesapmak
+import moduller.oyunlar
+import moduller.renkler
+import moduller.yazarlar
+
 def uygulamamenu():
     print("\033[1;32;40m")
     #print("╔"+"═"*20+"╗")
@@ -7,11 +14,11 @@ def uygulamamenu():
     print("║                     ║")
     print("║  1-Hesap makinesi   ║")
     print("║  2-Çizimler         ║")
-    print("║  3-Takvim           ║")
+    print("║  3-Aylar            ║")
     print("║  4-Oyunlar          ║")
     print("║  5-Burçlar          ║")
     print("║  6-Renkler          ║")
-    print("║  7-                 ║")
+    print("║  7-Yazarlar         ║")
     print("║  8-                 ║")
     print("║  9-                 ║")
     print("║  10-                ║")
@@ -19,12 +26,39 @@ def uygulamamenu():
     print("║    Seçimiz nedir?   ║")
     print("╚═════════════════════╝")
 
+    secim = input()
+
     if secim =="1":
-        print("Hesap makinesini seçtiniz")
-        uygulamalar.hesapmakinesi()
-        uygulamamenu
-    elif secim == "2":
-        print("Çizimler kategorisini seçtiniz") 
-    elif secim == "3":
-        print("Takvim kategorisini seçtiniz")
+        print("Hesap makinesi uygulamasını seçtiniz")
+        moduller.hesapmak.hesapmenu()
+        uygulamamenu()
+    elif secim =="2":
+        print("Çizimler uygulamasını seçtiniz.")
+        moduller.cizimler.cizimmenu()
+        uygulamamenu()
+    elif secim =="3":
+        print("Aylar uygulmasını seçtiniz.")
+        moduller.aylar.aylarmenu()
+        uygulamamenu()    
+    elif secim =="4":
+        print("Oyunlar uygulmasını seçtiniz.")
+        moduller.oyunlar.oyunlarmenu()
+        uygulamamenu()  
+    elif secim =="5":
+        print("Burçlar uygulmasını seçtiniz.")
+        moduller.burclar.burcmenu()
+        uygulamamenu()    
+    elif secim =="6":
+        print("Renkler uygulmasını seçtiniz.")
+        moduller.renkler.renklermenu()
+        uygulamamenu()    
+    else :
+        print("Yazarlar uygulmasını seçtiniz.")
+        moduller.yazarlar.yazarlarmenu()
+        uygulamamenu() 
+
+uygulamamenu()
+    
+
+    
         
